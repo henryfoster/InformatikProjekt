5e2a7a556bf1e0b781622702e48f16312f09f776
fhsdhf <fhsdhf@gmail.com>
2016-03-11 18:39:48 +0100

    Ergänzung mit Verlag, Kategorien, Datum, Klasse und Nachricht

--- server_php/buch.php
+++ server_php/buch.php
@@ -18,10 +18,45 @@ $bilder = bilder_zum_buch($buch['id']);
         <div id="buch-daten">
             <div id="buch-titel"><?php echo($buch['titel']); ?></div>
             <div id="buch-isbn">ISBN: <?php echo($buch['isbn']); ?></div>
+            <div id="buch-verlag">Verlag: <?php echo($buch['verlag']); ?></div>
             <div id="buch-jahr">Jahr: <?php echo($buch['jahr']); ?></div>
+            <div id="buch-kategorien">Fächer: <?php
+                for ($i = 0; $i < count($buch['kategorien']); $i++) {
+                    echo($buch['kategorien'][$i]);
+                    if ($i < count($buch['kategorien']) - 1)
+                        echo(', ');
+                }
+            ?></div>
+            <div id="buch-klasse">Klasse: <?php
+                if ($buch['klasse1'] == 0)
+                    echo("Nicht angegeben");
+                else if ($buch['klasse1'] == $buch['klasse2'])
+                    echo($buch['klasse1']);
+                else
+                    echo($buch['klasse1'] . ' - ' . $buch['klasse2']);
+            ?></div>
             <div id="buch-zustand"><?php echo($buch['zustand']); ?></div>
-            <div id="buch-kontakt">Kontakt: <?php echo($buch['kontakt']); ?></div>
+            <div id="buch-datum">Aufgegeben: <?php echo(date('d.m.Y', $buch['datum'])); ?></div>
             <div id="buch-preis">Preis: <b><?php echo($buch['preis']); ?> €</b></div>
+            
+            <?php
+                $query = $_GET;
+                $query[$_ARG_NACHRICHT] = '1';
+                $link = '?'.http_build_query($query);
+                if ($benutzer_eingeloggt) { ?>
+                <div id="buch-kontakt">
+                    Verkäufer: <?php echo($buch['kontakt']); ?><br>
+                    <form action='<?php echo $link; ?>' method='post'>
+                    <input type='hidden' name='nachricht-sender' value='<?php echo($_SESSION['userid']); ?>'></input>
+                    <input type='hidden' name='nachricht-ziel' value='<?php echo($buch['kontakt']); ?>'></input>
+                    <input type='hidden' name='nachricht-buch' value='<?php echo($buch['id']); ?>'></input>
+                    <input name='nachricht-text'></input>
+                    <input type='submit' value='Nachricht senden'>
+                    </form>
+                </div>
+            <?php } else { ?>
+                <div id="buch-kontakt">Um den Verkäufer zu kontaktieren, melden Sie sich bitte an.</div>
+            <?php } ?>
         </div>
     </div>
     <div id="buch-beschreibung">
