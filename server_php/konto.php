a009f6202df4e4e96f9aca875e0ac61b6b267baa
fhsdhf <fhsdhf@gmail.com>
2016-03-14 18:18:08 +0100

    Ergänzung mit Buchverwaltung

--- server_php/konto.php
+++ server_php/konto.php
@@ -2,4 +2,14 @@
 <?php
 echo ("<h2>".$_SESSION['userid']."</h2>");
 ?>
-...
+<br>
+<a href='buchauf.php'>Buch aufgeben</a>
+<br>
+<h2>Meine Anzeigen</h2>
+<?php
+$anz = meine_buecher($_SESSION['userid']);
+for ($i = 0; $i < count($anz); $i++) {
+    $a = $anz[$is];
+    echo("<div class='mein_buch'><a href='?buch=".$a['id']."'>".$a['titel']."</div>\n");
+}
+?>
