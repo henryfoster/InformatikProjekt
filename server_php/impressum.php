6fd5a5860a3cda461f9d221a580f01c4b875bec6
fhsdhf <fhsdhf@gmail.com>
2016-03-14 18:17:15 +0100

    Impressum

---
+++ server_php/impressum.php
@@ -0,0 +1,21 @@
+<meta http-equiv="content-type" content="text/html;charset=utf-8">
+<html lang="de">
+<head>
+<title>Buch aufgeben</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<?php
+include ('funktionen.php');
+include ('login.php');
+include('header.php');
+?>
+
+<div id='impressum-text'>
+blabla
+</div>
+
+
+<?php include ('unten.php'); ?>
+</body>
+</html>
