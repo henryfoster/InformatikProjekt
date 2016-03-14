43adb63333091781d12d7349b5ce38c4d2f4b466
fhsdhf <fhsdhf@gmail.com>
2016-03-14 22:36:46 +0100

    Bücher aufgeben

--- server_php/buchauf.php
+++ server_php/buchauf.php
@@ -11,6 +11,27 @@ include ('login.php');
 
 include('header.php');
 
+if(isset($_GET['buchauf']) && $_GET['buchauf'] == 1) {
+    $tt = $_POST['titel'];
+    $isbn = $_POST['isbn'];
+    $verl = $_POST['verlag'];
+    $jahr = $_POST['jahr'];
+    $besc = $_POST['beschreibung'];
+    $zust = $_POST['zustand'];
+    $email = $_SESSION['userid'];
+    $preis = $_POST['preis'];
+    $kat = $_POST['kategorien'];
+    $kl1 = $_POST['klasse1'];
+    $kl2 = $_POST['klasse2'];
+    
+    $erfolg = buch_aufgeben($tt, $isbn, $verl, $jahr, $besc, $zust, $email, $preis, $kat, $kl1, $kl2);
+
+    if ($erfolg) {
+        echo("Efolgreich");
+    } else {        
+        echo("Fehler");
+    }
+}
 
 ?>
 
@@ -23,7 +44,6 @@ Verlag: <input name="verlag"><br>
 Jahr: <input name="jahr"><br>
 Beschreibung: <input name="beschreibung"><br>
 Zustand: <input name="zustand"><br>
-Kontakt: <input name="kontakt"><br>
 Preis: <input name="preis"><br>
 Kategorien: <input name="kategorien"><br>
 Klasse1: <input name="klasse1"><br>
