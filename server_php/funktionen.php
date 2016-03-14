faa1abdfe1ae7be443756a9f127443de214f645c
fhsdhf <fhsdhf@gmail.com>
2016-03-14 22:37:46 +0100

    Ergänzung mit SQL-Abfragen, Suche, Verwaltung eigener Anzeigen

--- server_php/funktionen.php
+++ server_php/funktionen.php
@@ -13,22 +13,86 @@ Status: 0: aktiv, 1: deaktiviert 2: verkauft
 $kategorien = array ( 'Allgemein', 'Biologie', 'Chemie', 'Deutsch', 'Englisch', 'Französisch', 'Geographie', 'Geschichte', 'Informatik', 'Kunst', 'Mathematik', 'Musik', 'Physik', 'Politikwissenschaft', 'Russisch', 'Tafelwerk' );
 
 
+// Argumente in PHP: "seite.de/?q=suchbegriff&sort=0&kat=kategorie1..."
+// Details zum Buch: "seite.de/?q=suchbegriff&sort=0&kat=kategorie1...&buch=BuchID"
+
+// Argumente
+$_ARG_Q = "q";// Suchbegriff
+$_ARG_SORT = "sort";// Sortieren: 0: Name aufsteigend, 1: absteigend, 2: Preis auf ...
+$_ARG_KAT = "kat";// Suche in Kategorien
+$_ARG_SEITE = "p";// Nummer der Seite in Trefferliste
+$_ARG_BUCH = "buch";// Details zum Buch
+$_ARG_LOGIN = "login";// Ein- / Ausloggen
+$_ARG_NACHRICHT = "nachricht";
+
+// Abfrege der Argumente aus URL
+$arg_q = "";
+if (isset($_GET[$_ARG_Q]))
+    $arg_q = $_GET[$_ARG_Q];
+$arg_sort = "0";
+if (isset($_GET[$_ARG_SORT]))
+    $arg_sort = $_GET[$_ARG_SORT];
+$arg_kat = "";
+if (isset($_GET[$_ARG_KAT]))
+    $arg_kat = $_GET[$_ARG_KAT];
+$arg_seite = "";
+if (isset($_GET[$_ARG_SEITE]))
+    $arg_seite = $_GET[$_ARG_SEITE];
+$arg_buch = "";
+if (isset($_GET[$_ARG_BUCH]))
+    $arg_buch = $_GET[$_ARG_BUCH];
+if (isset($_GET[$_ARG_NACHRICHT])) {
+    unset($_GET[$_ARG_NACHRICHT]);
+    nachricht_senden($_POST['nachricht-sender'], $_POST['nachricht-ziel'], $_POST['nachricht-buch'], $_POST['nachricht-text']);
+}
+
+
+
+
 // Vor.: ...
 // Eff.: liefert die Liste der Ergebnisse ...
 function suche($q, $sort, $kat) {
+    $sql = "SELECT * FROM Buecher WHERE Status LIKE 0";
+    if ($kat != "" && $kat != 'alle') {
+        $sql .= " AND Kategorien LIKE '$kat'";
+    }
+
+    mysql_verbinden();
+    $query = mysql_query($sql);
+    mysql_close();
+
     $erg = array();
-    
-    // hier kommt die SQL-Anfrage ...
-    for ($i = 0; $i < 100; $i++) {
-        array_push($erg, randombuch ($q));
+
+    while ($row = mysql_fetch_array($query, MYSQL_ASSOC)) {
+        if ($row['ISBN'] == $q) {
+            array_push($erg, $row);
+        } else if ($q == '' || suche_im_titel($row['Titel'], $q)) {
+            array_push($erg, $row);
+        }
     }
     
     $erg = sortieren($erg, $sort);
     return $erg;
 }
+function suche_im_titel($titel, $suchbegriff) {
+    $t = strtolower($titel);
+    $qs = explode(' ', strtolower($suchbegriff));
+    
+    $begr_max = count($qs);
+    $begr_gefunden = 0;
+
+    foreach ($qs as $q) {
+        if (strpos($t, $q) !== false)
+            $begr_gefunden++;
+    }
+
+    return ($begr_gefunden > 0 && $begr_gefunden > $begr_max*0.8);
+}
 function suche_id($id) {
-    // sql ...
-    return randombuch("Buch " . $id);
+    mysql_verbinden();
+    $erg = mysql_fetch_array(mysql_query("SELECT * FROM Buecher WHERE BuchID = '$id'"));
+    mysql_close();
+    return $erg;
 }
 // Vor.: keine
 // Eff.: liefert die hochgeladenen Bilder zum Buch mit im Argument agbegebener ID
@@ -46,9 +110,9 @@ function bilder_zum_buch($buch_id) {
 // Vor.: Liste besteht aus Büchern
 // Eff.: liefert die sortierte Liste
 function sortieren($erg, $sort) {
-    $arg = "titel";// $sort 0 oder 1
+    $arg = "Titel";// $sort 0 oder 1
     if ($sort >= 2)
-        $arg = "preis";// $sort 2 oder 3
+        $arg = "Preis";// $sort 2 oder 3
     
     $erg = quicksort($erg, $arg);
     
@@ -105,59 +169,150 @@ function nachricht_senden($email_sender, $email_ziel, $buch_id, $text) {
 
 // eigene Bücher verwalten
 function meine_buecher($email) {
-    $b = array();
+    $sql = "SELECT * FROM Buecher WHERE EMail LIKE '$email'";
 
-    array_push($b, randombuch ($email));
-    array_push($b, randombuch ($email));
+    mysql_verbinden();
+    $query = mysql_query($sql);
+    mysql_close();
 
-    return $b;
+    $erg = array();
+
+    while ($row = mysql_fetch_array($query, MYSQL_ASSOC)) {
+        array_push($erg, $row);
+    }
+    
+    $erg = quicksort($erg, 'Datum');
+    $erg = array_reverse($erg);
+    return $erg;
 }
-function buch_aufgeben() {
-    return true;
+function buch_aufgeben($tt, $isbn, $verl, $jahr, $besc, $zust, $email, $preis, $kat, $kl1, $kl2) {
+    
+    $id = buch_neue_id();
+
+    $q = "INSERT INTO Buecher VALUES ('$id', '$tt', '$isbn', '$verl', $jahr, '$besc', '$zust', '$email', $preis, '$kat', $kl1, $kl2, 0, NOW());";
+    
+    mysql_verbinden();
+    $erfolg = mysql_query($q);
+    mysql_close();
+
+    return $erfolg;
+}
+function buch_neue_id() {
+    $id = rand(10000000, 99999999);
+    mysql_verbinden();
+    $q = "SELECT * FROM Buecher WHERE BuchID LIKE '$id'";
+    $erfolg = (mysql_num_rows(mysql_query($q)) == 0);
+    mysql_close();
+    if ($erfolg)
+        return $id;
+    return buch_neue_id();
+}
+
+
+
+// Registration
+function registrierung_email_pruefen($email) {
+    mysql_verbinden();
+    
+    $q = "SELECT * FROM Benutzer WHERE EMail LIKE '$email'";
+    
+    $erfolg = mysql_num_rows(mysql_query($q));
+
+    mysql_close();
+
+    return $erfolg != 0;
+}
+function registrierung($email, $passwort, $benutzer) {
+    mysql_verbinden();
+    
+    $q = "INSERT INTO Benutzer VALUES ('$email', '$benutzer', '$passwort', NOW());";
+    
+    $erfolg = mysql_query($q);
+
+    mysql_close();
+
+    return $erfolg;
+    
 }
 
 
 // SQL Tabellen erstellen
+function mysql_verbinden() {
+    $servername = "mysql1.000webhost.com";
+    $username = "a5059333_mfsb";
+    $password = "InfoProjekt2016";
+    $dbname = "a5059333_mfsb";
+    $servername = "localhost";
+    $username = "root";
+    $password = "pass";
+    $dbname = "mfsb";
+    
+    mysql_connect($servername, $username, $password);
+    mysql_select_db($dbname);
+}
 function mysql_test() {
-$servername = "localhost";
-$username = "root";
-$password = "pass";
-$dbname = "mfsb";
+    echo('Verbindung wird hergestellt...<br>');
+    mysql_verbinden();
+    echo('Verbindung ok<br>');
+    
+/*
+- Benutzer: EMail, Benutzername, Passwort, Datum
+- Bücher: BuchID, Titel, ISBN, Verlag, Jahr, Beschreibung, Zustand, EMail, Preis, Kategorien, Klasse1, Klasse2, Datum, Status
+- Bilder: BildID, BuchID
+- Nachrichten: NachrichtID, EMailSender EMailZiel, BuchID, Text, Gelesen, Datum
+*/
+    tabelle_erstellen('Benutzer', 'CREATE TABLE Benutzer (
+        EMail VARCHAR(50) NOT NULL,
+        Benutzername VARCHAR(30) NOT NULL,
+        Passwort VARCHAR(30) NOT NULL,
+        Datum TIMESTAMP
+    )');
+    tabelle_erstellen('Buecher', 'CREATE TABLE Buecher (
+        BuchID INT(10),
+        Titel VARCHAR(30) NOT NULL,
+        ISBN VARCHAR(30) NOT NULL,
+        Verlag VARCHAR(30) NOT NULL,
+        Jahr VARCHAR(30) NOT NULL,
+        Beschreibung TEXT NOT NULL,
+        Zustand VARCHAR(30) NOT NULL,
+        EMail VARCHAR(30) NOT NULL,
+        Preis INT(10),
+        Kategorien VARCHAR(30) NOT NULL,
+        Klasse1 INT(2),
+        Klasse2 INT(2),
+        Status INT(2),
+        Datum TIMESTAMP
+    )');
+    tabelle_erstellen('Bilder', 'CREATE TABLE Bilder (
+        BildID INT(10),
+        BuchID INT(10)
+    )');
+    tabelle_erstellen('Nachrichten', 'CREATE TABLE Nachrichten (
+        NachrichtID INT(10),
+        EMailSender VARCHAR(30) NOT NULL,
+        EMailZiel VARCHAR(30) NOT NULL,
+        BuchID INT(10),
+        Text TEXT NOT NULL,
+        Gelesen INT(1),
+        Datum TIMESTAMP
+    )');
+    
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    mysql_close();
 }
-
-$table = 'Benutzer';
-if(mysql_num_rows(mysql_query("SHOW TABLES LIKE '".$table."'"))==1) 
-    echo "Table exists";
-else echo "Table does not exist";
-
-// sql to create table
-$tabelle_benutzer = "CREATE TABLE Benutzer (
-email VARCHAR(50) NOT NULL,
-benutzername VARCHAR(30) NOT NULL,
-passwort VARCHAR(30) NOT NULL,
-reg_datum TIMESTAMP
-)";
-$tabelle = "CREATE TABLE Benutzer (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP
-)";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-
-$conn->close();
+function tabelle_erstellen($name, $sqlcode) {
+    if(mysql_num_rows(mysql_query("SHOW TABLES LIKE '$name'"))==1) {
+        echo "Tabelle $name existiert<br>OK<br>";
+    } else {
+        echo "Tabelle $name erstellen...<br>";
+        
+        if (mysql_query($sqlcode) === TRUE) {
+            echo "OK<br>";
+        } else {
+            echo "Fehler <br>";
+        }
+    }
+    
 }
 
 
