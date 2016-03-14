6f76b8545c921dd4128aaa902ae8f491f0135a62
fhsdhf <fhsdhf@gmail.com>
2016-03-14 22:39:10 +0100

    Anpassung für reelle Daten aus der Datenbank

--- server_php/index.php
+++ server_php/index.php
@@ -11,40 +11,6 @@ ini_set('display_errors', 'on');
 
 include ('funktionen.php');
 
-
-// Argumente in PHP: "seite.de/?q=suchbegriff&sort=0&kat=kategorie1..."
-// Details zum Buch: "seite.de/?q=suchbegriff&sort=0&kat=kategorie1...&buch=BuchID"
-
-// Argumente
-$_ARG_Q = "q";// Suchbegriff
-$_ARG_SORT = "sort";// Sortieren: 0: Name aufsteigend, 1: absteigend, 2: Preis auf ...
-$_ARG_KAT = "kat";// Suche in Kategorien
-$_ARG_SEITE = "p";// Nummer der Seite in Trefferliste
-$_ARG_BUCH = "buch";// Details zum Buch
-$_ARG_LOGIN = "login";// Ein- / Ausloggen
-$_ARG_NACHRICHT = "nachricht";
-
-// Abfrege der Argumente aus URL
-$arg_q = "";
-if (isset($_GET[$_ARG_Q]))
-    $arg_q = $_GET[$_ARG_Q];
-$arg_sort = "0";
-if (isset($_GET[$_ARG_SORT]))
-    $arg_sort = $_GET[$_ARG_SORT];
-$arg_kat = "";
-if (isset($_GET[$_ARG_KAT]))
-    $arg_kat = $_GET[$_ARG_KAT];
-$arg_seite = "";
-if (isset($_GET[$_ARG_SEITE]))
-    $arg_seite = $_GET[$_ARG_SEITE];
-$arg_buch = "";
-if (isset($_GET[$_ARG_BUCH]))
-    $arg_buch = $_GET[$_ARG_BUCH];
-if (isset($_GET[$_ARG_NACHRICHT])) {
-    unset($_GET[$_ARG_NACHRICHT]);
-    nachricht_senden($_POST['nachricht-sender'], $_POST['nachricht-ziel'], $_POST['nachricht-buch'], $_POST['nachricht-text']);
-}
-
 // ...
 
 $ergebnisse_pro_seite = 20;
@@ -69,6 +35,16 @@ include ('header.php');
 
 <div id="kategorie-liste">
 <?php
+$query = $_GET;
+$query[$_ARG_KAT] = "alle";
+$query[$_ARG_BUCH] = "";
+if ($arg_buch != "")
+    $query[$_ARG_SEITE] = "";
+$link = '?'.http_build_query($query);
+echo ("<div class='kategorie'");
+    if ($arg_kat == '' || $arg_kat == 'alle')
+        echo (" id='kategorie-ausgewaehlt'");
+    echo ("><a href='$link'><span>Alle</span></a></div>\n");
 for ($i = 0; $i < count($kategorien); $i++) {
     $kategorie = $kategorien[$i];
     
@@ -102,6 +78,12 @@ if ($arg_buch != "") {
 } else if ($benutzer_eingeloggt && $arg_q == "" && $arg_kat == "") {
     include ('konto.php');
 } else {
+    
+    if (count($ergebnisse) == 0) {
+        echo('Keine Bücher gefunden');
+    }
+
+
     // Suchergebnisse anzeigen
     
     $ergebnisse = array_slice($ergebnisse, $arg_seite*$ergebnisse_pro_seite, $ergebnisse_pro_seite);
@@ -109,33 +91,33 @@ if ($arg_buch != "") {
     
     for ($i = 0; $i < $ergebnisse_pro_seite && $i < count($ergebnisse); $i++) {
         $erg = $ergebnisse[$i];
-        $beschr = $erg['beschreibung'];
+        $beschr = $erg['Beschreibung'];
         if (strlen($beschr) > 100)
             $beschr = substr($beschr, 0, 100) . '...';
         
         
         // PHP Argumente
         $query = $_GET;
-        $query[$_ARG_BUCH] = $erg['id'];
+        $query[$_ARG_BUCH] = $erg['BuchID'];
         $link = '?'.http_build_query($query);
         
-        $bilder = bilder_zum_buch($erg['id']);
+        $bilder = bilder_zum_buch($erg['BuchID']);
         $bild = $bilder[0];
         
         echo "<a href='$link'>"
             . "<div class='ergebnis'>\n"
             . "  <div class='erg-bild'><img src='$bild' /></div>\n"
             . "  <div class='erg-1'>\n"
-            . "    <div class='erg-title'>".$erg['titel']."</div>\n"
+            . "    <div class='erg-title'>".$erg['Titel']."</div>\n"
             . "    <div>\n"
-            . "      <span class='erg-jahr'>".$erg['jahr']."</span>\n"
-            . "      <span class='erg-zustand'>".$erg['zustand']."</span>\n"
+            . "      <span class='erg-jahr'>".$erg['Jahr']."</span>\n"
+            . "      <span class='erg-zustand'>".$erg['Zustand']."</span>\n"
             . "    </div\n>"
             . "    <div class='erg-beschreibung'>$beschr</div>\n"
             . "  </div>\n"
             . "  <div class='erg-2'>\n"
-            . "    <div class='erg-preis'>".$erg['preis']." €</div>\n"
-            . "    <div class='erg-isbn'>".$erg['isbn']."</div>\n"
+            . "    <div class='erg-preis'>".$erg['Preis']." €</div>\n"
+            . "    <div class='erg-isbn'>".$erg['ISBN']."</div>\n"
             . "  </div>\n"
             . "</div></a>\n";
     }
