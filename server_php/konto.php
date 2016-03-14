e04126d191cf1770db4cefd1b6cf54659dedfce2
fhsdhf <fhsdhf@gmail.com>
2016-03-14 22:39:28 +0100

    Ergänzung mit eingenen Anzeigen

--- server_php/konto.php
+++ server_php/konto.php
@@ -1,6 +1,7 @@
-<h1>Konto</h1>
+<div id='konto'>
+<div id='konto-titel'><h1>Konto</h1></div>
 <?php
-echo ("<h2>".$_SESSION['userid']."</h2>");
+echo ("<div id='konto-benutzer'><h3>".$_SESSION['userid']."<br>".benutzername($_SESSION['userid'])."</h3></div>");
 ?>
 <br>
 <a href='buchauf.php'>Buch aufgeben</a>
@@ -8,8 +9,20 @@ echo ("<h2>".$_SESSION['userid']."</h2>");
 <h2>Meine Anzeigen</h2>
 <?php
 $anz = meine_buecher($_SESSION['userid']);
+if (count($anz) == 0) {
+    echo("Keine aufgegebene Bücher");
+}
 for ($i = 0; $i < count($anz); $i++) {
     $a = $anz[$i];
-    echo("<div class='mein_buch'><a href='?buch=".$a['id']."'>".$a['titel']."</div>\n");
+    $status = $a['Status'];
+    if ($status == 0)
+        $status = 'Aktiv';
+    else if ($status == 0)
+        $status = 'Nicht aktiv';
+    echo("<div class='mein-buch'><a href='?buch=".$a['BuchID']."'><b>".$a['Titel']."</b><br>
+            Aufgegeben: ".$a['Datum']."<br>
+            Status: $status  <a href='.'><button class='mein-buch-deaktivieren'>deaktivieren</button></a></div>\n");
 }
 ?>
+
+</div>
