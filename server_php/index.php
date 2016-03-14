5948e52fc99b170285778647912715ebb0f7b0a3
fhsdhf <fhsdhf@gmail.com>
2016-03-14 18:17:32 +0100

    Unterer Teil eingefügt

--- server_php/index.php
+++ server_php/index.php
@@ -147,6 +147,9 @@ if ($arg_buch != "") {
 </div>
 
 
+<?php include ('unten.php'); ?>
+
+
 <!-- START OF HIT COUNTER CODE --><br><script language="JavaScript" src="http://www.counter160.com/js.js?img=14"></script><br><a href="https://www.000webhost.com"><img src="http://www.counter160.com/images/14/left.png" alt="Free web hosting" border="0" align="texttop"></a><a href="http://www.hosting24.com"><img alt="Web hosting" src="http://www.counter160.com/images/14/right.png" border="0" align="texttop"></a><!-- END OF HIT COUNTER CODE -->
 
 </body>
