358b2ff88e9283e6228845640098238e9cc93763
dugma <ggglol@inbox.com>
2016-03-16 00:06:22 +0100

    Added files via upload
    zur sicherheit mit 2 betitelt, damit ein backup der alten dateien vorhanden ist

---
+++ server_php/buchauf2.php
@@ -0,0 +1,104 @@
+<head><meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+
+<title>Buch aufgeben</title>
+<link rel="stylesheet" type="text/css" href="Buch%20aufgeben-Dateien/style.css">
+</head>
+
+
+<body>
+
+ 
+
+<div id="header">
+
+    <div id="logo"><a href="http://mfsb.site88.net/"><img src="Markt%20f%C3%BCr%20Schulb%C3%BCcher-Dateien/logo.png"></a></div>
+    
+    <div id="header-text">
+    <div id="header-oben">
+        <div id="login">
+                                <form action="?q=&amp;login=1" method="post">
+                    E-Mail: <input name="email" type="email">
+                    Passwort: <input name="passwort" type="password">
+                    <input value="Abschicken" type="submit">
+                    </form>
+                    <a href="http://mfsb.site88.net/registrierung.php">Registrierung</a>
+                    </div>
+    </div>
+
+    <div id="header-unten">
+        <!-- Form zur Suche "/?q=eingabe" -->
+        <form name="suche" method="GET" action=".">
+            <!-- Eingabefeld -->
+            <input id="suchbegriff" class="header-item" placeholder="Suchbegriff oder ISBN" name="q" autofocus="" type="text">
+            <!-- Taste mit Bild und Text -->
+            <button type="submit" id="suchtaste" class="header-item">
+                <img src="Markt%20f%C3%BCr%20Schulb%C3%BCcher-Dateien/suche.png">
+                <span>Suchen</span>
+            </button>
+        
+        </form>
+            
+            
+                    
+        <div class="header-item" id="ergebnisliste-steuerung">
+                    </div>
+        </div>
+        
+    </div>
+
+</div>
+
+<div id="main">
+
+<div id="kategorie-liste">
+<div class="kategorie" id="kategorie-ausgewaehlt"><a href="http://mfsb.site88.net/?q=&amp;kat=alle&amp;buch="><span>Alle</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Allgemein&amp;buch="><span>Allgemein</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Biologie&amp;buch="><span>Biologie</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Chemie&amp;buch="><span>Chemie</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Deutsch&amp;buch="><span>Deutsch</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Englisch&amp;buch="><span>Englisch</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Franz%C3%B6sisch&amp;buch="><span>Französisch</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Geographie&amp;buch="><span>Geographie</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Geschichte&amp;buch="><span>Geschichte</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Informatik&amp;buch="><span>Informatik</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Kunst&amp;buch="><span>Kunst</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Mathematik&amp;buch="><span>Mathematik</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Musik&amp;buch="><span>Musik</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Physik&amp;buch="><span>Physik</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Politikwissenschaft&amp;buch="><span>Politikwissenschaft</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Russisch&amp;buch="><span>Russisch</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Tafelwerk&amp;buch="><span>Tafelwerk</span></a></div>
+
+</div>
+
+
+
+
+</div>
+    
+<div id="buch-aufgeben">
+
+<form action="?buchauf=1" method="post">
+<div id="bucha">
+Titel: <input name="titel"><br>
+ISBN: <input name="isbn"><br>
+Verlag: <input name="verlag"><br>
+Jahr: <input name="jahr"><br>
+Beschreibung: <input name="beschreibung"><br>
+Zustand: <input name="zustand"><br>
+Preis: <input name="preis"><br>
+Kategorien: <input name="kategorien"><br>
+Klasse1: <input name="klasse1"><br>
+Klasse2: <input name="klasse2"><br>
+<input value="Abschicken" type="submit">
+</div></form></div>
+
+
+
+
+
+<script type="text/javascript" src="Buch%20aufgeben-Dateien/count.php"></script>
+<!-- End Of Analytics Code -->
+
+</body>
---
+++ server_php/registrierung2.php
@@ -0,0 +1,100 @@
+<html class="" lang="de"><head><meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+  <title>Registrierung</title>	
+</head> 
+<body>
+ 
+
+<div id="header">
+
+    <div id="logo"><a href="http://mfsb.site88.net/"><img src="Markt%20f%C3%BCr%20Schulb%C3%BCcher-Dateien/logo.png"></a></div>
+    
+    <div id="header-text">
+    <div id="header-oben">
+        <div id="login">
+                                <form action="?q=&amp;login=1" method="post">
+                    E-Mail: <input name="email" type="email">
+                    Passwort: <input name="passwort" type="password">
+                    <input value="Abschicken" type="submit">
+                    </form>
+                    <a href="http://mfsb.site88.net/registrierung.php">Registrierung</a>
+                    </div>
+    </div>
+
+    <div id="header-unten">
+        <!-- Form zur Suche "/?q=eingabe" -->
+        <form name="suche" method="GET" action=".">
+            <!-- Eingabefeld -->
+            <input id="suchbegriff" class="header-item" placeholder="Suchbegriff oder ISBN" name="q" autofocus="" type="text">
+            <!-- Taste mit Bild und Text -->
+            <button type="submit" id="suchtaste" class="header-item">
+                <img src="Markt%20f%C3%BCr%20Schulb%C3%BCcher-Dateien/suche.png">
+                <span>Suchen</span>
+            </button>
+        
+        </form>
+            
+            
+                    
+        <div class="header-item" id="ergebnisliste-steuerung">
+                    </div>
+        </div>
+        
+    </div>
+
+</div>
+
+<div id="main">
+
+<div id="kategorie-liste">
+<div class="kategorie" id="kategorie-ausgewaehlt"><a href="http://mfsb.site88.net/?q=&amp;kat=alle&amp;buch="><span>Alle</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Allgemein&amp;buch="><span>Allgemein</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Biologie&amp;buch="><span>Biologie</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Chemie&amp;buch="><span>Chemie</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Deutsch&amp;buch="><span>Deutsch</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Englisch&amp;buch="><span>Englisch</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Franz%C3%B6sisch&amp;buch="><span>Französisch</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Geographie&amp;buch="><span>Geographie</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Geschichte&amp;buch="><span>Geschichte</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Informatik&amp;buch="><span>Informatik</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Kunst&amp;buch="><span>Kunst</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Mathematik&amp;buch="><span>Mathematik</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Musik&amp;buch="><span>Musik</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Physik&amp;buch="><span>Physik</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Politikwissenschaft&amp;buch="><span>Politikwissenschaft</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Russisch&amp;buch="><span>Russisch</span></a></div>
+<div class="kategorie"><a href="http://mfsb.site88.net/?q=&amp;kat=Tafelwerk&amp;buch="><span>Tafelwerk</span></a></div>
+
+</div>
+
+</div>
+
+<div id="unten">
+<a href="#">nach oben</a>
+<a href="http://mfsb.site88.net/impressum.php">Impressum</a>
+
+<!-- START OF HIT COUNTER CODE --><br><script language="JavaScript" src="Markt%20f%C3%BCr%20Schulb%C3%BCcher-Dateien/js.htm"></script><a href="http://www.counter160.com/" target="_blank"><img alt="Get your free hit counter!" src="Markt%20f%C3%BCr%20Schulb%C3%BCcher-Dateien/counter.htm" border="0"></a><br><a href="https://www.000webhost.com/"><img src="Markt%20f%C3%BCr%20Schulb%C3%BCcher-Dateien/left.png" alt="Free web hosting" align="texttop" border="0"></a><a href="http://www.hosting24.com/"><img alt="Web hosting" src="Markt%20f%C3%BCr%20Schulb%C3%BCcher-Dateien/right.png" align="texttop" border="0"></a><!-- END OF HIT COUNTER CODE -->
+</div>
+
+<script type="text/javascript" src="Markt%20f%C3%BCr%20Schulb%C3%BCcher-Dateien/count.php"></script>
+<!-- End Of Analytics Code -->
+
+
+<div id="register">
+<form style="" action="?register=1" method="post">
+<div id="registera">
+E-Mail:<br>
+<input name="email" type="email"><br><br> 
+
+Benutzername:<br>
+<input style="outline: 1px solid rgb(255, 0, 0); -moz-outline-radius: 5px;" name="benutzer"><br><br> 
+
+Passwort: <br>
+<input name="passwort" type="password"><br>
+
+Passwort wiederholen: <br>
+<input style="outline: 1px solid rgb(255, 0, 0); -moz-outline-radius: 5px;" name="passwort2" type="password"><br><br>
+ 
+<input value="Abschicken" type="submit">
+</div></form></div>
+</body><style type="text/css"></style></html>
