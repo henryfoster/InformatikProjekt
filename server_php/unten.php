b69d4626efb3fd9bf985911752ff2229e3dd9026
fhsdhf <fhsdhf@gmail.com>
2016-03-15 20:47:52 +0100

    Counter hier reingemacht

--- server_php/unten.php
+++ server_php/unten.php
@@ -1,4 +1,6 @@
 <div id='unten'>
 <a href='#'>nach oben</a>
 <a href='impressum.php'>Impressum</a>
+
+<!-- START OF HIT COUNTER CODE --><br><script language="JavaScript" src="http://www.counter160.com/js.js?img=14"></script><br><a href="https://www.000webhost.com"><img src="http://www.counter160.com/images/14/left.png" alt="Free web hosting" border="0" align="texttop"></a><a href="http://www.hosting24.com"><img alt="Web hosting" src="http://www.counter160.com/images/14/right.png" border="0" align="texttop"></a><!-- END OF HIT COUNTER CODE -->
 </div>
